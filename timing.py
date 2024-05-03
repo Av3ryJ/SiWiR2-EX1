e45fb2b8a950f5dcbad58952ff64977d700cf79d
Maximilian Fielitz <maximilian@fielitz.org>
2024-05-03 18:07:18 +0200

    timing.py should work

--- timing.py
+++ timing.py
@@ -1,36 +1,54 @@
 import subprocess
 import os.path as p
 import json
-import regex
+import re
 
 binary = "./mgsolve"
 levels = [3, 4, 5, 6, 7, 8]
 number_of_iterations = 10
 
 json_path = "times.json"
-time_json = {level: 0.0 for level in levels}
+time_json = {level: {} for level in levels}
 
 
 def run_bin(level):
     result = subprocess.run([binary, str(level), str(number_of_iterations)], capture_output=True, text=True)
     return result.stdout
 
+
 def parse_output(output):
-    return output
+    output_format = {"iterations": 0, "time": 0.0, "norms": []}
+    regex_string_lvl_iter = r"Level: (\d+).* Iterations: (\d*)"
+    regex_string_time = r"runtime: ([\d|\.]*)"
+
+    temp = re.search(regex_string_lvl_iter, output)
+    # output_format["level"] = int(temp.group(1))
+    output_format["iterations"] = int(temp.group(2))
+    temp = re.search(regex_string_time, output)
+    output_format["time"] = float(temp.group(1))
+
+    for i in range(1, output_format["iterations"]+1):
+        search_str = rf"iteration {i}: discrete residual norm = (.*);"
+        temp = re.search(search_str, output)
+        output_format["norms"].append(float(temp.group(1)))
+
+    print(output_format)
+    return output_format
+
 
 def time_all():
     # iterate over sizes and threads
-    for thread_num in thread_numbers:
-        print(f"running: {thread_num}")
-        returned = run_bin(str(thread_num))
+    for level in levels:
+        print(f"running: {level}")
+        returned = run_bin(str(level))
         parsed = parse_output(returned)
-        print(f"time was: {time}")
-        time_json[thread_num] = time
+        time_json[level] = parsed
     with open(json_path, 'w') as f:
         json.dump(time_json, f)
 
 
 if __name__ == '__main__':
+
     if p.exists(json_path):
         with open(json_path, 'r') as file:
             time_json = json.load(file)
